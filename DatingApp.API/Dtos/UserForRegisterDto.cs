5e064f7adce3e3cf85d3478b3bb3627d8ea41e53
Heitor Ramos <heitoralves07@hotmail.com>
2020-09-18 17:15:05 -0300

    Authentication/Class/Interface  and  UserModel/Register/Login

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="Pass must be bigger than 4 and lower than 8")]
+        public string Password { get; set; }
+
+    }
+}
